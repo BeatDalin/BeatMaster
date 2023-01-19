2c4aa215f7444cb90caf1edb511ed70b0463b420
전동현 <70741953+jundonghyun@users.noreply.github.com>
2023-01-19 17:44:52 +0900

    BTDI-36 [Add] Add SettingPopUp Panel
    [Add] Add UIManager
    [Edit] Edit ButtonScale.cs file

---
+++ BeatMasterProject/Assets/Scripts/MenuTitle/UIManager.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    
+    public Stack<GameObject> popUpStack;
+    public GameObject canvas;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        popUpStack = new Stack<GameObject>();
+        
+        InitCanvas(SceneManager.GetActiveScene().name);
+    }
+
+    public void InitCanvas(string SceneName)
+    {
+        string canvasName = SceneName + "Canvas";
+
+        canvas = GameObject.Find(canvasName);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (popUpStack.Count == 0)
+            {
+                return;
+            }
+            
+            GameObject g = popUpStack.Pop();
+            g.GetComponent<RectTransform>().DOLocalMove(new Vector3(Screen.width, 0, 0), 0.4f).onComplete += () =>
+            {
+                Destroy(g);
+            };
+        }
+    }
+}
