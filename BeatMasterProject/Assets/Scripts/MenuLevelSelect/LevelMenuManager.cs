b7fd0a09866c0442d23af15c32e5f835d2eac6b7
Seo <108641066+wk-seo@users.noreply.github.com>
2023-01-26 12:33:53 +0900

    BTDI-68 [Add] LevelMenu 레벨 선택 및 카메라 이동
    - SelectLevel: 판때기(LvFlat) 터치 시 GameScene으로 이동
    - Zoom: 핀치/alt+마우스 휠로 줌
    - MoveCamByDrag: 드래그로 카메라 이동
    - ClampPosition: 카메라 위치 제한

--- BeatMasterProject/Assets/Scripts/MenuLevelSelect/LevelMenuManager.cs
+++ BeatMasterProject/Assets/Scripts/MenuLevelSelect/LevelMenuManager.cs
@@ -1,38 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LevelMenuManager : MonoBehaviour
 {
-    private Vector3 _mouseDownPos;
+    [SerializeField] private GameObject _cam;
+    [SerializeField] private GameObject _camPosOrigin;
     
-    //cam variables..
-    [SerializeField] private float _rotateSpeed = 500.0f;
-    [SerializeField] private float _zoomSpeed = 10.0f;
-    [SerializeField] private float _scrollSpeed = 2000.0f;
-    [SerializeField] private float _dragSpeed = 30.0f;
-    [SerializeField] private Vector3 _camPosOrigin;
-    [SerializeField] private float _fovOrigin = 30f;
-    [SerializeField] private GameObject cam;
+    [SerializeField] private int _fovOrigin = 30;
+    [SerializeField] private int _fovMax = 36;
+    [SerializeField] private int _fovMin = 10;
     
-    private Camera mainCamera;
-    
-    private Vector2 _clickPoint;
+    [SerializeField] private float _xPosMax = 6f;
+    [SerializeField] private float _xPosMin = 2f;
 
-    void Start()
+    [SerializeField] private float _zPosMax = -6.5f;
+    [SerializeField] private float _zPosMin = -15f;
+
+    private Camera _mainCam;
+    
+    private Vector2 _touchPoint;
+    
+    private float _zoomSpeed = 10f;
+    private float _dragSpeed = 10f;
+
+    private void Start() 
     {
-        _camPosOrigin = new Vector3(2.7f, 7.47f, -10.23f);
-        mainCamera = cam.GetComponent<Camera>();
-        mainCamera.transform.position = _camPosOrigin;
-        mainCamera.fieldOfView = _fovOrigin;
+        // Init Main Camera's Pos, FOV
+        _mainCam = _cam.GetComponent<Camera>();
+        _mainCam.transform.position = _camPosOrigin.transform.position;
+        _mainCam.fieldOfView = _fovOrigin;
     }
 
-    void Update()
+    private void Update()
     {
+        SelectLevel();
+        Zoom();
+        MoveCamByDrag();
+    }
+
+    /// <summary>
+    /// LvFlat 오브젝트 터치 시 GameScene으로 이동
+    /// </summary>
+    private void SelectLevel()
+    {
+    #if UNITY_ANDROID
+
+        if(Input.touchCount == 1)
+        {
+            _touchPoint = new Vector2(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+            
+            Ray ray = _mainCam.ScreenPointToRay(_touchPoint);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Load GameScene
+            }
+        }
+
+    #else
         if (Input.GetMouseButtonDown(0))
         {
-            _mouseDownPos = Input.mousePosition;
-            Ray ray = Camera.main.ScreenPointToRay(_mouseDownPos);
+            _touchPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            Ray ray = _mainCam.ScreenPointToRay(_touchPoint);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit))
@@ -40,43 +73,110 @@ public class LevelMenuManager : MonoBehaviour
                 Debug.Log(hit.collider.name);
                 //Load GameScene
             }
+        }
 
-            else
+    #endif
+    }
+
+    /// <summary>
+    /// Zoom In, Out
+    /// 모바일 핀치, StandAlone Mouse Wheel
+    /// </summary>
+    private void Zoom()
+    {
+    #if UNITY_ANDROID
+
+        if(Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0); // 첫 번째 손가락 좌표
+            Touch touchOne = Input.GetTOuch(1); // 두 번째 손가락 좌표
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            float distance = (prevTouchDeltaMag - touchDeltaMag) * _zoomSpeed;
+            if (distance != 0)
             {
-                Zoom();
-                MoveCamToMousePosition();
+                _mainCam.fieldOfView += distance;
+                _mainCam.fieldOfView = Mathf.Clamp(_mainCam.fieldOfView, _fovMin, _fovMax); // 줌 최대, 최소 범위
             }
         }
     }
 
-    void Zoom()
-    {
-        //왼쪽 AltKey 누르면서 마우스 스크롤하면 zoom In, out
+    #else
         if (Input.GetKey(KeyCode.LeftAlt))
         {
             float distance = Input.GetAxis("Mouse ScrollWheel") * -1 * _zoomSpeed;
             if (distance != 0)
             {
-                mainCamera.fieldOfView += distance;
+                var fieldOfView = _mainCam.fieldOfView;
+                fieldOfView += distance;
+                //_mainCam.fieldOfView = fieldOfView;
+                _mainCam.fieldOfView = Mathf.Clamp(fieldOfView, _fovMin, _fovMax); // 줌 최대, 최소 범위
             }
         }
     }
+    #endif
 
-    void MoveCamToMousePosition()
+    private void MoveCamByDrag()
     {
-        _clickPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        Vector3 _goalPos = Camera.main.ScreenToViewportPoint((Vector2)Input.mousePosition - _clickPoint);
+#if UNITY_ANDROID
 
-        _goalPos.z = _goalPos.y;
-        _goalPos.y = .0f;
-            
-        //먼 move임..? 나중에 변수명 수정해야할듯
-        Vector3 move = _goalPos * (Time.deltaTime * _dragSpeed);
+        if(Input.GetTouch(0).phase == TouchPhase.Moved) // 바른 조건인지 확실치 않음! 테스트 필
+        {
+            Vector3 goalPos = _mainCam.ScreenToViewportPoint((Vector2)Input.GetTouch(0).position - _touchPoint);
 
-        float y = cam.transform.position.y;
-            
-        cam.transform.Translate(move);
-        cam.transform.position = new Vector3(cam.transform.position.x, y, cam.transform.position.z);
+            goalPos.z = goalPos.y;
+            goalPos.y = .0f;
+
+            Vector3 camMove = goalPos * (Time.deltaTime * _dragSpeed);
+
+            float y = cam.transform.position.y;
+
+            cam.transform.Translate(camMove);
+
+            // y축 값 고정
+            Vector3 position = cam.transform.position;
+            position = new Vector3(position.x, y, position.z);
+            cam.transform.position = ClampPoisition(position);
+        }
+
+#else
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 goalPos = _mainCam.ScreenToViewportPoint((Vector2)Input.mousePosition - _touchPoint);
+
+            goalPos.z = goalPos.y;
+            goalPos.y = .0f;
+
+            Vector3 camMove = goalPos * (Time.deltaTime * _dragSpeed);
+
+            float y = _cam.transform.position.y;
+
+            _cam.transform.Translate(camMove);
+
+            // y축 값 고정
+            Vector3 position = _cam.transform.position;
+            position = new Vector3(position.x, y, position.z);
+            _cam.transform.position = ClampPoisition(position);
+        }
+        
+#endif
     }
 
+    /// <summary>
+    /// Vector3 값의 최댓, 최솟값을 제한하는 함수
+    /// </summary>
+    /// <param name="position"></param> 제한할 Vector3
+    /// <returns></returns> 제한된 Vector3
+    private Vector3 ClampPoisition(Vector3 position)
+    {
+        return new Vector3(
+            Mathf.Clamp(position.x, _xPosMin, _xPosMax), 
+            position.y, 
+            Mathf.Clamp(position.z, _zPosMin, _zPosMax));
+    }
 }
