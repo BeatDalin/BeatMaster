75c261f30eca88c2d83c45b21d43b91f169216c4
Seo <108641066+wk-seo@users.noreply.github.com>
2023-01-26 17:22:27 +0900

    BTDI-19 [Edit] Voxel Resource 정리
    - Transform만 들어있는 voxel 모델의 껍데기 삭제 후 프리팹화했습니다.
    - Texture를 두 유형으로 간소화했습니다.
    +) BuildingManager.cs 파일만 생성함. 추후 별도 Commit 예정

---
+++ BeatMasterProject/Assets/Scripts/MenuLevelSelect/BuildingManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    // 작업 예정
+    private void Start()
+    {
+    }
+
+    public void SetMaterialColor()
+    {
+    }
+}
