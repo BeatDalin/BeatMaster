daa26aae558eef4c5644e2c85421557872ba0cf1
전동현 <70741953+jundonghyun@users.noreply.github.com>
2023-01-26 17:12:09 +0900

    BTDI-75 [ADD] Gain Star Animation
    PR 타입(하나 이상의 PR 타입을 선택해주세요)
    [V] 기능 추가
    [V] 기능 삭제
    [V] 버그 수정
    [] 의존성, 환경 변수, 빌드 관련 코드 업데이트
    반영 브랜치
    ui -> prototype
    
    변경 사항
    - 게임결과화면에서 점수별로 별의 개수를 보여주는 기능 추가 및 애니메이션 추가
    - 메인타이틀씬에서 버튼이 애니메이션 때문에 음악에 맞춰 버튼이 커지는 기능이 동작하지 않는 현상을 수정
    - 메인타이틀씬에서 버튼이 커진 상태에서 버튼을 클릭하면 다시 원래 버튼크기로 돌아가지 않는 현상을 수정
    - UI매니저에서 esc를 눌렀을때 스택에서 빼는 동작을 삭제 -> 이 기능이 필요한 스크립트에서 UI매니저에 접근해서 ClosePopUp함수를 부르도록 변경
    
    테스트 결과
    - 버튼의 크기가 커졌을때 여러번 클릭해도 클릭애니메이션이 동작하지 않도록해서 버튼크기가 돌아가지 않는 현상이 사라졌음

--- BeatMasterProject/Assets/Scripts/MenuTitle/MenuTitleButton.cs
+++ BeatMasterProject/Assets/Scripts/MenuTitle/MenuTitleButton.cs
@@ -39,10 +39,7 @@ public class MenuTitleButton : MonoBehaviour
     {
         Koreographer.Instance.RegisterForEvents("MenuBGMTrack", ChangeScale);
 
-        _buttons[(int)ButtonName.Stage].onClick.AddListener(() => SceneMoveBtn("Stage"));
-        _buttons[(int)ButtonName.Store].onClick.AddListener(() => OpenPopUp("Store"));
-        _buttons[(int)ButtonName.Setting].onClick.AddListener(() => OpenPopUp("Setting"));
-        _buttons[(int)ButtonName.ShutDown].onClick.AddListener(() => OpenPopUp("ShutDown"));
+        AddClickListener();
     }
 
     private void Update()
@@ -54,6 +51,82 @@ public class MenuTitleButton : MonoBehaviour
                 _simpleMusicPlayer.Play();
             }
         }
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UIManager.instance.ClosePopUp();
+        }
+    }
+
+    /// <summary>
+    /// 각 버튼에 클릭 리스너를 달아주는 함수
+    /// 눌린것을 표현하기 위해서 DOScale을 사용하고 완료되면 DORewind로 원래 Scale로 돌아오게함
+    /// </summary>
+    private void AddClickListener()
+    {
+        _buttons[(int)ButtonName.Stage].onClick.AddListener(() =>
+        {
+            if (_buttons[(int)ButtonName.Stage].transform.localScale == new Vector3(1, 1, 1))
+            {
+                _buttons[(int)ButtonName.Stage].transform.DOScale(new Vector3(0.9f, 0.9f, 0), 0.1f).onComplete += () =>
+                {
+                    _buttons[(int)ButtonName.Stage].transform.DORewind();
+                    SceneMoveBtn("Stage");
+                };
+            }
+            else
+            {
+                SceneMoveBtn("Stage");
+            }
+        });
+        
+        _buttons[(int)ButtonName.Store].onClick.AddListener(() =>
+        {
+            if (_buttons[(int)ButtonName.Store].transform.localScale == new Vector3(1, 1, 1))
+            {
+                _buttons[(int)ButtonName.Store].transform.DOScale(new Vector3(0.9f, 0.9f, 0), 0.1f).onComplete += () =>
+                {
+                    _buttons[(int)ButtonName.Store].transform.DORewind();
+                    OpenPopUp("Store");
+                };
+            }
+            else
+            {
+                OpenPopUp("Store");
+            }
+        });
+        
+        _buttons[(int)ButtonName.Setting].onClick.AddListener(() =>
+        {
+            if (_buttons[(int)ButtonName.Setting].transform.localScale == new Vector3(1, 1, 1))
+            {
+                _buttons[(int)ButtonName.Setting].transform.DOScale(new Vector3(0.9f, 0.9f, 0), 0.1f).onComplete += () =>
+                {
+                    _buttons[(int)ButtonName.Setting].transform.DORewind();
+                    OpenPopUp("Setting");
+                };
+            }
+            else
+            {
+                OpenPopUp("Setting");
+            }
+        });
+        
+        _buttons[(int)ButtonName.ShutDown].onClick.AddListener(() =>
+        {
+            if (_buttons[(int)ButtonName.ShutDown].transform.localScale == new Vector3(1, 1, 1))
+            {
+                _buttons[(int)ButtonName.ShutDown].transform.DOScale(new Vector3(0.9f, 0.9f, 0), 0.1f).onComplete += () =>
+                {
+                    _buttons[(int)ButtonName.ShutDown].transform.DORewind();
+                    OpenPopUp("ShutDown");
+                };
+            }
+            else
+            {
+                OpenPopUp("ShutDown");
+            }
+        });
     }
 
     private void OpenPopUp(string popUpName)
@@ -63,17 +136,11 @@ public class MenuTitleButton : MonoBehaviour
         switch (popUpName)
         {
             case "Store":
-                _storePopUp.SetActive(true);
-                _settingPopUp.GetComponent<RectTransform>().localPosition = new Vector3(Screen.width, 0, 0);
-                
-                UIManager.instance.popUpStack.Push(_settingPopUp);
+                UIManager.instance.OpenPopUp(_storePopUp);
                 break;
             
             case "Setting":
-                _settingPopUp.SetActive(true);
-                _settingPopUp.GetComponent<RectTransform>().localPosition = new Vector3(Screen.width, 0, 0);
-                
-                UIManager.instance.popUpStack.Push(_settingPopUp);
+                UIManager.instance.OpenPopUp(_settingPopUp);
                 break;
             
             case "ShutDown":
