508825a3272e0571b585a7c01e2817c4276c38bf
Seo <108641066+wk-seo@users.noreply.github.com>
2023-01-30 10:48:01 +0900

    BTDI-82 [Edit] 빌딩 모델 보여주기
    - Building: 레벨 오브젝트가 가지고 있는 스크립트. 성취도 및 Clear여부에 따라 모델을 보여주는 함수를 가지고 있음(ShowBuilding, ShowStar)
    - BuildingManager: 현재 스테이지에서 Clear한 레벨 중 가장 큰 값을 가져와서(GetMaxLevelInStage), 해당 레벨과 다음 레벨의 모델을 보여준다.

---
+++ BeatMasterProject/Assets/Scripts/MenuLevelSelect/Building.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Building : MonoBehaviour
+{
+    [SerializeField] private GameObject _flat;
+    [SerializeField] private GameObject _model;
+    [SerializeField] private GameObject _starCanvas;
+    [SerializeField] private Image[] starImg;
+
+    public void ShowBuilding(bool isClear)
+    {
+        Instantiate(this);
+        _flat.GetComponent<MeshRenderer>().enabled = true;
+        _flat.GetComponent<BoxCollider>().enabled = !isClear;
+        _model.GetComponent<MeshRenderer>().enabled = isClear;
+        _starCanvas.GetComponent<Canvas>().enabled = isClear;
+    }
+
+    public void ShowStar(int star)
+    {
+        for (int i = 0; i < star; i++)
+        {
+            starImg[i].enabled = true;
+        }
+    }
+}
+   
+    
