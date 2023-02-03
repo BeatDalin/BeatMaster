54c5a2701cb394493cb21a94bd05fe852c2e914a
Seo <108641066+wk-seo@users.noreply.github.com>
2023-02-03 12:24:14 +0900

    [Edit] SwipeList, StorePopUp 소스 코드 수정
    - Update에서 생성하던 float 변수를 Start에서 생성하도록 수정
    - 지역 변수 distance 전역 변수로 수정
    - using Unity.VisualScripting 삭제

--- BeatMasterProject/Assets/02. Scripts/PopUp/SwipeList.cs
+++ BeatMasterProject/Assets/02. Scripts/PopUp/SwipeList.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,14 +9,18 @@ public class SwipeList : MonoBehaviour
     public GameObject scrollbar;
     private float _scrollPos = 0;
     private float[] _pos;
-    void Update()
+    private float _distance;
+
+    void Start()
     {
         _pos = new float[transform.childCount];
-        float distance = 1f / (_pos.Length - 1f);
-        
+        _distance = 1f / (_pos.Length - 1f);
+    }
+    void Update()
+    {
         for (int i = 0; i < _pos.Length; i++)
         {
-            _pos[i] = distance * i;
+            _pos[i] = _distance * i;
         }
 
         if (Input.GetMouseButton(0))
@@ -27,7 +32,7 @@ public class SwipeList : MonoBehaviour
         {
             for (int i = 0; i < _pos.Length; i++)
             {
-                if (_scrollPos < _pos[i] + (distance / 2) && _scrollPos > _pos[i] - (distance / 2))
+                if (_scrollPos < _pos[i] + (_distance / 2) && _scrollPos > _pos[i] - (_distance / 2))
                 {
                     scrollbar.GetComponent<Scrollbar>().value =
                         Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, _pos[i], 0.1f);
