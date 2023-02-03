b0ce827721153ba5896e1b56a13d2e98741c839e
Seo <108641066+wk-seo@users.noreply.github.com>
2023-02-03 12:06:02 +0900

    [Edit] 변수명 수정
    - SwipeList.cs 안의 private 변수 이름에 _를 붙였습니다.. ^^

--- BeatMasterProject/Assets/02. Scripts/PopUp/SwipeList.cs
+++ BeatMasterProject/Assets/02. Scripts/PopUp/SwipeList.cs
@@ -6,31 +6,31 @@ using UnityEngine.UI;
 public class SwipeList : MonoBehaviour
 {
     public GameObject scrollbar;
-    private float scroll_pos = 0;
-    private float[] pos;
+    private float _scrollPos = 0;
+    private float[] _pos;
     void Update()
     {
-        pos = new float[transform.childCount];
-        float distance = 1f / (pos.Length - 1f);
-        for (int i = 0; i < pos.Length; i++)
+        _pos = new float[transform.childCount];
+        float distance = 1f / (_pos.Length - 1f);
+        
+        for (int i = 0; i < _pos.Length; i++)
         {
-            pos[i] = distance * i;
+            _pos[i] = distance * i;
         }
 
         if (Input.GetMouseButton(0))
         {
-            scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
+            _scrollPos = scrollbar.GetComponent<Scrollbar>().value;
         }
 
         else
         {
-            for (int i = 0; i < pos.Length; i++)
+            for (int i = 0; i < _pos.Length; i++)
             {
-                if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
+                if (_scrollPos < _pos[i] + (distance / 2) && _scrollPos > _pos[i] - (distance / 2))
                 {
                     scrollbar.GetComponent<Scrollbar>().value =
-                        Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[i], 0.1f);
-                    
+                        Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, _pos[i], 0.1f);
                 }
             }
         }
