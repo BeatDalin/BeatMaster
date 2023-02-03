91562aa6434b9fe54dc44f9a69679fda86db9520
전동현 <70741953+jundonghyun@users.noreply.github.com>
2023-02-03 17:34:09 +0900

    BTDI-120 [FIX] TouchEffect
    PR 타입(하나 이상의 PR 타입을 선택해주세요)
    [] 기능 추가
    [] 기능 삭제
    [] 버그 수정
    [V] 의존성, 환경 변수, 빌드 관련 코드 업데이트
    반영 브랜치
    ui -> prototype
    
    변경 사항
    오브젝트풀 생성방식 변경
    터치이펙트가 다른 씬에서 적용되지 않는 현상 수정

--- BeatMasterProject/Assets/02. Scripts/Pools/TouchEffectPool.cs
+++ BeatMasterProject/Assets/02. Scripts/Pools/TouchEffectPool.cs
@@ -6,6 +6,7 @@ public class TouchEffectPool : ObjectPooling
 {
     public override void Init()
     {
+        DontDestroyOnLoad(this);
         for (int i = 0; i < initCount; i++)
         {
             poolingObjectQueue.Enqueue(CreateNewObject());
