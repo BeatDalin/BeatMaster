1b054e0b6dd355c5db2e9ab1a18944a94a9667ee
juchan <jjc6252498@gmail.com>
2023-02-08 10:07:47 +0900

    BTDI-149 [Update] PR을 받기위한 Commit
    BTDI-149 [Update] PR을 받기위한 Commit

--- BeatMasterProject/Assets/02. Scripts/LevelGame/ChangeSprites/ResourcesChanger.cs
+++ BeatMasterProject/Assets/02. Scripts/LevelGame/ChangeSprites/ResourcesChanger.cs
@@ -5,49 +5,25 @@ using SonicBloom.Koreo;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class SpriteChanger : MonoBehaviour
+public class ResourcesChanger : MonoBehaviour
 {
     [SerializeField] private ChangingResources[] _changingResources;
     private BackgroundMover _backgroundMover;
     private CameraController _cameraController;
-    private CharacterMovement _characterMovement;
-    private SpriteRenderer _characterRenderer;
+    private Material _backgroundMaterial;
     private int _materialIndex;
-    private int _spriteIndex;    
-    
-    // TODO
-    // 추후에 적, 주변 환경 등 1가지 이상으로 여러가지가 달라진다면 (적, 타일, {이펙트도 여기..??} 등)
-    // enum과 딕셔너리를 사용하는 방법으로 구현하는 것을 생각해보았는데
-    // SerializeField로는 어려우니 Resource.Load를 사용해야 될 것 같다.
-    // => ResourceManager 고려
-    /*public enum ChangingSprites
-    {
-        
-    }*/
-    
-    // 배경은 Quad로 만들어졌으니 따로 Material 변수로 작성하는 것은 맞는 것 같다.
-    // => SerializedField 가능
-    
-    // TODO
-    // NormalGame도 등록할 이벤트들을 폴더별로 나누어 다 불러오든지 enum으로 나누어 씬 별로 불러와서 등록하던지 해야할 듯 싶다.
-    // -> 상의해보기
-    // NormalGame에서 성공했는지 여부도 받아야 함
-    // -> 지은님과 상의
-    // Rewind시 다시 바꾸는 로직도 생각해봐야 함
-    // 기능 명세서와 클래스 다이어그램, 지라까지 싹 갈아 엎는 거 상의
-    
+    private bool _isSpeedUp;    
     
     private void Awake()
     {
-        _characterMovement = FindObjectOfType<CharacterMovement>();
-        _characterRenderer = _characterMovement.GetComponentInChildren<SpriteRenderer>();
         _backgroundMover = FindObjectOfType<BackgroundMover>();
         _cameraController = FindObjectOfType<CameraController>();
+        _backgroundMaterial = _backgroundMover.GetComponent<Renderer>().material;
+        Init();
     }
 
-    public void Init()
+    private void Init()
     {
-
         switch (SceneLoadManager.Instance.Scene)
         {
             case SceneLoadManager.SceneType.Level1:
@@ -80,6 +56,7 @@ public class SpriteChanger : MonoBehaviour
     private void ResetTexturesOffset(Material[] materials)
     {
         // BackgroundMover에서 이동한 결과에 의해 원본이 훼손되는 것을 막기위함
+        _backgroundMaterial.mainTextureOffset = Vector2.zero;
         foreach (var material in materials)
         {
             material.mainTextureOffset = Vector2.zero;
@@ -88,6 +65,7 @@ public class SpriteChanger : MonoBehaviour
 
     public void OnSpeedChanged()
     {
+        _isSpeedUp = !_isSpeedUp;
         ChangeBackgroundMaterial();
         SetBackgroundSize();
     }
@@ -100,7 +78,7 @@ public class SpriteChanger : MonoBehaviour
                 // 스프라이트 교체
                 _materialIndex++;
                 _materialIndex %= _changingResources[0].ChangingMaterials.Length;
-                Material changingMaterial = _changingResources[0].ChangingMaterials[_materialIndex];
+                Material changingMaterial = _isSpeedUp? _changingResources[0].ChangingMaterials[_materialIndex] : _backgroundMaterial;
                 _backgroundMover.SetTextureOffset(changingMaterial);
                 break;
             case SceneLoadManager.SceneType.Level2:
@@ -118,32 +96,5 @@ public class SpriteChanger : MonoBehaviour
         backgroundTrans.localScale = backgroundTrans.localScale.x.Equals(_cameraController.MinOrthoSize)
             ? backgroundTrans.localScale * _cameraController.MinOrthoSize / _cameraController.MaxOrthoSize
             : backgroundTrans.localScale * _cameraController.MaxOrthoSize / _cameraController.MinOrthoSize;
-    }
-
-    public void OnLongPressed()
-    {
-        // 플레이어 스프라이트 변경
-        switch (SceneLoadManager.Instance.Scene)
-        {
-            case SceneLoadManager.SceneType.Level1:
-                // 스프라이트 교체
-                _spriteIndex++;
-                _spriteIndex %= _changingResources[0].ChangingSprites.Length;
-                Sprite changingSprite = _changingResources[0].ChangingSprites[_spriteIndex];
-                SetSprite(changingSprite);
-                break;
-            case SceneLoadManager.SceneType.Level2:
-                break;
-            case SceneLoadManager.SceneType.Level3:
-                break;
-            case SceneLoadManager.SceneType.Level4:
-                break;
-        }
-    }
-    
-    private void SetSprite(Sprite sprite)
-    {
-        Debug.Log("스프라이트 변경");
-        _characterRenderer.sprite = sprite;
     }
 }
--- BeatMasterProject/Assets/02. Scripts/LevelGame/ScriptableObjects/ChangingResources.cs
+++ BeatMasterProject/Assets/02. Scripts/LevelGame/ScriptableObjects/ChangingResources.cs
@@ -6,10 +6,8 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(menuName = "SpritesForm", fileName = "Level")]
 public class ChangingResources : ScriptableObject
 {
-    [SerializeField] private Material[] _changingMaterials;
-    public Material[] ChangingMaterials { get => _changingMaterials; private set => _changingMaterials = value; }
-    [SerializeField] private Sprite[] _changingSprites;
-    public Sprite[] ChangingSprites { get => _changingSprites; private set => _changingSprites = value; }
+    [SerializeField] private Material[] changingMaterials;
+    public Material[] ChangingMaterials { get => changingMaterials; private set => changingMaterials = value; }
     [SerializeField] private Monster[] _changingMonsters;
     public Monster[] ChangingMonsters { get => _changingMonsters; private set => _changingMonsters = value; }
     [SerializeField] private ParticleSystem[] _changingParticles;
