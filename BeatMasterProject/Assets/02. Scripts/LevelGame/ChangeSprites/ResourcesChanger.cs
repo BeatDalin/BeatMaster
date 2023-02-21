2a7348906e46de3af5b878679600095ef0b449b4
juchan <jjc6252498@gmail.com>
2023-02-21 16:18:46 +0900

    BTDI-215 [Fix] Background Texture get transparent bug
    문제점 : 배경의 Shader Graph Offset 이동에서 (quad였다가 2D square로 바꿈 => 중첩 배경을 위해) 텍스처가 투명해지는 버그가 있었다.
    해결 방안 : Texure의 Mesh Type을 Full Rect로 해주면 해결된다.

--- BeatMasterProject/Assets/02. Scripts/LevelGame/ChangeSprites/ResourcesChanger.cs
+++ BeatMasterProject/Assets/02. Scripts/LevelGame/ChangeSprites/ResourcesChanger.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
-using UnityEngine.Serialization;
 
 public class ResourcesChanger : MonoBehaviour
 {
