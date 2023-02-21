31201925308ee216d22051b73641af853996b0eb
전동현 <70741953+jundonghyun@users.noreply.github.com>
2023-02-22 08:47:09 +0900

    BTDI-222 [Fix] JudgeText
    - 판정 텍스트가 보이지 않는 현상 수정

--- BeatMasterProject/Assets/02. Scripts/LevelGame/NormalGame.cs
+++ BeatMasterProject/Assets/02. Scripts/LevelGame/NormalGame.cs
@@ -128,6 +128,7 @@ public class NormalGame : Game
             CheckBeatResult(shortResult, shortIdx, isShortKeyCorrect, _pressedTime, _eventRangeShort);
             mapTemp.shortTileParticleList[shortIdx].GetComponent<Note>().beatResult = shortResult[shortIdx].ToString();
             rewindTime.RecordRewindPoint(characterMovement.transform.position, shortResult[shortIdx].ToString());
+            gameUI.ChangeOutLineColor(shortResult[shortIdx]);
             //gameUI.ChangeOutLineColor(shortResult[shortIdx]);
             shortIdx++;
             if (!isShortKeyCorrect)
@@ -172,7 +173,7 @@ public class NormalGame : Game
             CheckBeatResult(shortResult, shortIdx, isShortKeyCorrect, _pressedTime, _eventRangeShort);
             mapTemp.shortTileParticleList[shortIdx].GetComponent<Note>().beatResult = shortResult[shortIdx].ToString();
             rewindTime.RecordRewindPoint(characterMovement.transform.position, shortResult[shortIdx].ToString());
-            //gameUI.ChangeOutLineColor(shortResult[shortIdx]);
+            gameUI.ChangeOutLineColor(shortResult[shortIdx]);
             monsterPooling.DisableMonster();
             _isShortVisited[shortIdx] = true;
             if (!isRewinding)
