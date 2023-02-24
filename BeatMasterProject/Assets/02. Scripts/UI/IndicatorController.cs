21b2d97d82be1bcbcd9019939db92619d85ac03d
juchan <jjc6252498@gmail.com>
2023-02-24 14:22:39 +0900

    BTDI-234 [Update] indicator Event callbacks update
    인디케이터의 이벤트 콜백이 업데이트 되었습니다.
    - 처음에 안내 창이 우측에서 나올 수 있도록 하였습니다.
    - 다음 타일을 이벤트를 받아와서 다음 타일에 대한 애니메이션만 실행하도록 하였습니다.

--- BeatMasterProject/Assets/02. Scripts/UI/IndicatorController.cs
+++ BeatMasterProject/Assets/02. Scripts/UI/IndicatorController.cs
@@ -1,14 +1,81 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using SonicBloom.Koreo;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IndicatorController : MonoBehaviour
 {
-    [SerializeField] private Transform[] _indicatorsTransforms;
-    [SerializeField] private float _waitTime;
+    [SerializeField] [EventID] private string _indicatorStart;
+    [SerializeField] [EventID] private string _indicator;
+    [SerializeField] private float _moveAmount = 0.1f;
+
+    private float _startPosX;
+    private float _goalPosX;
+    private int _preIndex;
+    private RectTransform _canvasRectTrans;
+    private List<RectTransform> _indicatorsTransforms = new List<RectTransform>();
+    private Dictionary<int, Animator> _animatorDict = new Dictionary<int, Animator>();
+
     private void Awake()
     {
+        Init();
+    }
+
+    private void Init()
+    {
+        _canvasRectTrans = GetComponent<RectTransform>();
+        Koreographer.Instance.RegisterForEvents(_indicatorStart, StartIndicating);
+        Koreographer.Instance.RegisterForEvents(_indicator, PlayNextAnim);
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            RectTransform targetRectTrans = transform.GetChild(i).GetComponent<RectTransform>();
+            _indicatorsTransforms.Add(targetRectTrans);
+            _goalPosX = targetRectTrans.anchoredPosition.x;
+            _startPosX = _goalPosX + _canvasRectTrans.rect.width;
+            targetRectTrans.anchoredPosition = new Vector2(_startPosX, targetRectTrans.anchoredPosition.y);
+            
+            _animatorDict.Add(i, _indicatorsTransforms[i].GetComponentInChildren<Animator>());
+            _animatorDict[i].enabled = false;
+        }
+        
+    }
+
+    private void PlayNextAnim(KoreographyEvent evt)
+    {
+        int index = evt.GetIntValue();
+        _animatorDict[_preIndex].Rebind();
+        // _animatorDict[_preIndex].Update(0f);
+        
+        _animatorDict[_preIndex].enabled = false;
+        _animatorDict[index].enabled = true;
+        
+        _preIndex = index;
+    }
+
+    private void StartIndicating(KoreographyEvent evt)
+    {
+        int value = evt.GetIntValue();
+        StartCoroutine(CoMoveIndicatingBox(value));
+    }
+
+    private IEnumerator CoMoveIndicatingBox(int index)
+    {
+        RectTransform targetTrans = _indicatorsTransforms[index];
+        float ratio = 0f;
+        
+        while (ratio < 1)
+        {
+            // currentPos알기
+            ratio += Time.deltaTime * _moveAmount;
+            float currentPosX = Mathf.Lerp(_startPosX, _goalPosX, ratio);
+            targetTrans.anchoredPosition = new Vector2(currentPosX, targetTrans.anchoredPosition.y);
+            
+            yield return null;
+        }
+        
         
     }
 
