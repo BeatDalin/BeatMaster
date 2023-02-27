b0bb010dcae7053371dd1b16954db430c30ac4eb
Seo <108641066+wk-seo@users.noreply.github.com>
2023-02-27 15:49:21 +0900

    [Edit] delete debug.log
    - 주석 처리

--- BeatMasterProject/Assets/02. Scripts/LevelGame/DataCenter.cs
+++ BeatMasterProject/Assets/02. Scripts/LevelGame/DataCenter.cs
@@ -33,7 +33,7 @@ public class DataCenter : MonoBehaviour
     private void Start()
     {
         // if you want to find where the json file is located...
-        Debug.Log(Application.persistentDataPath);
+        // Debug.Log(Application.persistentDataPath);
         // CreateData();
     }
 
